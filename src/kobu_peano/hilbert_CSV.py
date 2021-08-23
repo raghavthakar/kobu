8dab747ac673fb695af666f94090f1896709e6f0
Akshat Pandey <akshatpandeyplus41@gmail.com>
2021-08-23 11:16:30 +0530

    Add files via upload

---
+++ src/kobu_peano/hilbert_CSV.py
@@ -0,0 +1,43 @@
+from turtle import *
+import time
+import csv
+
+with open('robot1_hilbert.csv','w',newline='') as robot1_csv_file
+    robot1_writer=csv.writer(robot1_csv_file)
+
+def hilbert(level, angle, step):
+
+	# Input Parameters are numeric
+	# Return Value: None
+	if level == 0:
+        forward(length)
+        print(position()[0],position()[1])
+
+        with open('robot1_hilbert.csv','a',newline='') as robot1_csv_file
+            robot1_writer=csv.writer(robot1_csv_file)
+            robot1_writer=writerow([position()[0],position()[1])
+		return
+
+	right(angle)
+	hilbert(level-1, -angle, step)
+
+	forward(step)
+	left(angle)
+	hilbert(level-1, angle, step)
+
+	forward(step)
+	hilbert(level-1, angle, step)
+
+	left(angle)
+	forward(step)
+	hilbert(level-1, -angle, step)
+	right(angle)
+
+
+
+penup()
+goto(-100,100)
+pendown()
+right(-45)
+	# For positioning turtle
+hilbert(4,90,size/(2**level-1))
